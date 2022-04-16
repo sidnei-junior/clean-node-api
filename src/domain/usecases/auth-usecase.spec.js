7887f06f56bae9718b04a81a09e290456f6674f2
sidneiarauj <sidnei.ifrn@gmail.com>
2022-04-16 18:12:07 -0300

    refectory code

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -40,16 +40,14 @@ const makeSut = () => {
 describe('Auth UseCase', () => {
   test('should throw if no email is provided', async () => {
     const { sut } = makeSut()
-    await expect(async () => {
-      await sut.auth()
-    }).rejects.toThrow(new MissingParamError('email'))
+    const promise = sut.auth()
+    await expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if no password is provided', async () => {
     const { sut } = makeSut()
-    await expect(async () => {
-      await sut.auth('any_email@mail.com')
-    }).rejects.toThrow(new MissingParamError('password'))
+    const promise = sut.auth('any_email@mail.com')
+    await expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   test('Should call LoadUserByEmailRepository with correct email', async () => {
@@ -60,16 +58,14 @@ describe('Auth UseCase', () => {
 
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase()
-    await expect(async () => {
-      await sut.auth('any_email@mail.com', 'any_password')
-    }).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+    const promise = sut.auth('any_email@mail.com', 'any_password')
+    await expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
   })
 
   test('Should throw if LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
-    await expect(async () => {
-      await sut.auth('any_email@mail.com', 'any_password')
-    }).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+    const promise = sut.auth('any_email@mail.com', 'any_password')
+    await expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
   })
 
   test('Should return null if LoadUserByEmailRepository returns null', async () => {
