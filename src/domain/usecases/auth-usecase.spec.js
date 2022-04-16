dc371503d46ba61c1e581f115eba6a898f1b0581
sidneiarauj <sidnei.ifrn@gmail.com>
2022-04-16 18:02:48 -0300

    ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -18,7 +18,10 @@ class AuthUseCase {
     if (!this.loadUserByEmailRepository.load) {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 const makeSut = () => {
@@ -67,5 +70,11 @@ describe('Auth UseCase', () => {
     await expect(async () => {
       await sut.auth('any_email@mail.com', 'any_password')
     }).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+    await expect(accessToken).toBeNull()
   })
 })
